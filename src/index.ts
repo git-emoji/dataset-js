8afa78fc7dacb0ecff8cfcb049b94fb0b5daf8a6
Babak K. Shandiz <babak.k.shandiz@gmail.com>
2022-09-15 14:44:44 +0430

    🎁 Add word data
    Signed-off-by: Babak K. Shandiz <babak.k.shandiz@gmail.com>

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
 import { emoji as _emoji } from './emoji';
 import { context as _context } from './context';
+import { word as _word } from './word';
 
 export const emoji = _emoji;
 export const context = _context;
+export const word = _word;
