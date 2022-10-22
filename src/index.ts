dd1ef5552e3b7aa93613098cb04b9a9e746c8cf6
Babak K. Shandiz <babak.k.shandiz@gmail.com>
2022-10-22 21:27:57 +0330

    📦 Update exported types
    Signed-off-by: Babak K. Shandiz <babak.k.shandiz@gmail.com>

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,11 @@
 import { emoji as _emoji } from './emoji';
 import { context as _context } from './context';
-import { word as _word } from './word';
+import { word as _word, WordTag as _WordTag, Word as _Word, WordEntry as _WordEntry } from './word';
 
 export const emoji = _emoji;
 export const context = _context;
 export const word = _word;
+
+export type WordTag = _WordTag;
+export type Word = _Word;
+export type WordEntry = _WordEntry;
