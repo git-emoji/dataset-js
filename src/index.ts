95982178d4342719691f2fabfc42e95f05bcd1d2
Babak K. Shandiz <babak.k.shandiz@gmail.com>
2022-12-12 00:11:31 +0330

    🔨 Add `contextV1` to exports for better DX
    Signed-off-by: Babak K. Shandiz <babak.k.shandiz@gmail.com>

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 import * as e from './emoji';
-import * as c from './context';
+import * as c1 from './context';
 import * as c2 from './context_v2';
 import * as w from './word';
 
@@ -7,8 +7,9 @@ export type EmojiDict = e.EmojiDict;
 export type EmojiEntry = e.EmojiEntry;
 export const emoji = e.emoji;
 
-export type ContextEntry = c.ContextEntry;
-export const context = c.context;
+export type ContextEntry = c1.ContextEntry;
+export const context = c1.context;
+export const contextV1 = c1.context;
 
 export const contextV2 = c2.contextV2;
 
