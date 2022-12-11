d25be3be8d579ddfd0e6a8399a100790f2fe2ec8
Babak K. Shandiz <babak.k.shandiz@gmail.com>
2022-12-12 00:01:08 +0330

    🔨 Update exposed symbols
    Signed-off-by: Babak K. Shandiz <babak.k.shandiz@gmail.com>

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import * as e from './emoji';
 import * as c from './context';
+import * as c2 from './context_v2';
 import * as w from './word';
 
 export type EmojiDict = e.EmojiDict;
@@ -8,6 +9,8 @@ export const emoji = e.emoji;
 
 export type ContextEntry = c.ContextEntry;
 export const context = c.context;
+
+export const contextV2 = c2.contextV2;
 
 export type WordTag = w.WordTag;
 export type WordDict = w.WordDict;
